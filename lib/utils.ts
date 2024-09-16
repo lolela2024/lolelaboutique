89013541dcd1101a04060112c107901229690d22
trixtu <trixtu12@gmail.com>
2024-09-16 19:01:36 +0200

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,6 +14,7 @@ export function slugify(text:string) {
     .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')             // Trim - from start of text
-    .replace(/-+$/, '');            // Trim - from end of text
+    .replace(/-+$/, '');   
+             // Trim - from end of text
 }
 
