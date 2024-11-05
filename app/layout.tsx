c1697a684bc7e330cca889a4a865392a15f1221f
trixtu <trixtu12@gmail.com>
2024-11-05 18:52:12 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default async function RootLayout({
   const session = await auth();
 
   return (
+    // test commit
     <SessionProvider session={session}>
       <html lang="ro">
         <body className={lora.className}>
